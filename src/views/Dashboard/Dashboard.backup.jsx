1a41475c0127b7c944284fa235376dd36b5541a8
Gorka <gorkavillara@gmail.com>
2022-10-10 11:31:56 +0200

    🎨 Tema 10 - Empezando con MUI

---
+++ src/views/Dashboard/Dashboard.backup.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styles from "./Dashboard.module.scss";
+// import "./Dashboard.module.scss";
+
+import { Outlet, NavLink } from "react-router-dom";
+import DashboardContextProvider from "../../contexts/DashboardContextProvider";
+
+const Dashboard = () => {
+  return (
+    <DashboardContextProvider>
+      <div className={styles.Dashboard}>
+        <div className={styles.sidebar}>
+          <span className={styles.logo}>SpeedRanking</span>
+          <ul>
+            <li>
+              <NavLink to="/dashboard/top">Top</NavLink>
+            </li>
+            <li>
+              <NavLink to="/dashboard/detalles">Detalles</NavLink>
+            </li>
+            <li>
+              <NavLink to={{ pathname: "/dashboard/usuarios" }}>
+                Usuarios
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/dashboard/registro">Registro</NavLink>
+            </li>
+            <li>
+              <NavLink to="/dashboard/configuracion">Configuración</NavLink>
+            </li>
+            <li>
+              <NavLink to="/dashboard/perfil">Perfil</NavLink>
+            </li>
+            <li>
+              <NavLink to="/dashboard/login">Login</NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.contenido}>
+          <Outlet />
+        </div>
+      </div>
+    </DashboardContextProvider>
+  );
+};
+
+export default Dashboard;
--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,42 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Dashboard.module.scss";
 // import "./Dashboard.module.scss";
 
-import { Outlet, NavLink } from "react-router-dom";
+import {
+  Drawer,
+  IconButton,
+  Typography,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  Box,
+  Divider,
+  ListItemText,
+  Fab,
+} from "@mui/material";
+
+import { FaFlagCheckered } from "react-icons/fa";
+import { CgDetailsMore } from "react-icons/cg";
+import { FiUsers, FiLogIn } from "react-icons/fi";
+import { BsChevronLeft } from "react-icons/bs";
+
+import { Outlet, NavLink, Link } from "react-router-dom";
 import DashboardContextProvider from "../../contexts/DashboardContextProvider";
 
 const Dashboard = () => {
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <DashboardContextProvider>
       <div className={styles.Dashboard}>
-        <div className={styles.sidebar}>
-          <span className={styles.logo}>SpeedRanking</span>
-          <ul>
-            <li>
-              <NavLink to="/dashboard/top">Top</NavLink>
-            </li>
-            <li>
-              <NavLink to="/dashboard/detalles">Detalles</NavLink>
-            </li>
-            <li>
-              <NavLink to={{ pathname: "/dashboard/usuarios" }}>
-                Usuarios
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/dashboard/registro">Registro</NavLink>
-            </li>
-            <li>
-              <NavLink to="/dashboard/configuracion">Configuración</NavLink>
-            </li>
-            <li>
-              <NavLink to="/dashboard/perfil">Perfil</NavLink>
-            </li>
-            <li>
-              <NavLink to="/dashboard/login">Login</NavLink>
-            </li>
-          </ul>
-        </div>
+        <Drawer
+          variant="persistent"
+          open={isOpen}
+          sx={{ width: isOpen ? "200px" : "0px" }}
+        >
+          <Box sx={{ padding: "15px" }}>
+            <Typography variant="h5">Dashboard MUI</Typography>
+          </Box>
+          <Divider />
+          <List>
+            <ListItem>
+              <Link to="/dashboard/top" style={{ textDecoration: "none", color: "inherit" }}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <FaFlagCheckered />
+                  </ListItemIcon>
+                  <ListItemText primary="Top" />
+                </ListItemButton>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link to="/dashboard/detalles" style={{ textDecoration: "none", color: "inherit" }}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <CgDetailsMore />
+                  </ListItemIcon>
+                  <ListItemText primary="Detalles" />
+                </ListItemButton>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link to="/dashboard/usuarios" style={{ textDecoration: "none", color: "inherit" }}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <FiUsers />
+                  </ListItemIcon>
+                  <ListItemText primary="Usuarios" />
+                </ListItemButton>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link to="/dashboard/login" style={{ textDecoration: "none", color: "inherit" }}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <FiLogIn />
+                  </ListItemIcon>
+                  <ListItemText primary="Login" />
+                </ListItemButton>
+              </Link>
+            </ListItem>
+          </List>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <IconButton onClick={() => setIsOpen(false)}>
+              <BsChevronLeft />
+            </IconButton>
+          </Box>
+        </Drawer>
+        {!isOpen && <Fab color="primary" onClick={() => setIsOpen((prev) => !prev)} sx={{ position: "absolute", left: "10px", bottom: "10px" }}>
+          {isOpen ? (
+            <BsChevronLeft />
+          ) : (
+            <BsChevronLeft style={{ transform: "rotate(180deg)" }} />
+          )}
+        </Fab>}
         <div className={styles.contenido}>
           <Outlet />
         </div>
