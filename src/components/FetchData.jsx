a65b07f21976b2a7a316a12ff31d86e15f1bea5b
Gorka <gorkavillara@gmail.com>
2022-10-14 12:57:28 +0200

    📕 Tema 13 - Librería custom añadida

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Button, Input } from "gv-components-formacion";
 
 // const url =
 //   process.env.NODE_ENV === "production"
 //     ? "https://rickandmortyapi.com/api/character/2"
 //     : "https://rickandmortyapi.com/api/character/5";
 
-const url = process.env.REACT_APP_API_URL
+const url = process.env.REACT_APP_API_URL;
 
 const FetchData = () => {
   const [info, setInfo] = useState();
@@ -22,6 +23,8 @@ const FetchData = () => {
       <span>{info.data.name}</span>
       <br />
       <span>Entorno: {process.env.NODE_ENV}</span>
+      <br />
+      <Button primary={false} rgb size="large" label="Botón custom" />
     </div>
   ) : null;
 };
